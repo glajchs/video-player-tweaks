8b3955b18824cb659d6fe1bafda09a42e76855c3
glajchs <glajchs@users.noreply.github.com>
2026-06-14 20:30:53 -0400

    Fix the comments so it works again

--- src/tweaks/youtubetv.js
+++ src/tweaks/youtubetv.js
@@ -1,4 +1,5 @@
-javascript:(function(){
+javascript:(function() {
+    /* NOTE: Since bookmarklets inline the whole script into 1 single line, ALL comments must be of this format */
     function removeYoutubePlayerStuff() {
       document.querySelector(".ypl-layout").childNodes.forEach(function(childNode) {
           if (childNode.nodeName == "DIV" && (childNode.classList == null || !childNode.classList.contains("ypl-main"))) {
@@ -7,7 +8,7 @@ javascript:(function(){
       });
     }
     removeYoutubePlayerStuff();
-    // Youtube adds the divs back once or twice, but running it again fixes it.
+    /* Youtube adds the divs back once or twice, but running it again fixes it. */
     setTimeout(removeYoutubePlayerStuff, 500);
     setTimeout(removeYoutubePlayerStuff, 1500);
 })();
